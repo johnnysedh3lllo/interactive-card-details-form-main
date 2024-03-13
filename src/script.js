0851e941a6b4ed2479088a8307615cb7be81c0e1
johnnysedh3lllo <johnnysaidh3lllo@gmail.com>
2024-03-13 12:32:40 +0100

    design successfully implemented

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// CARD PROPERTIES
+const cardNumber = document.querySelector(".card__number");
+const cardName = document.querySelector(".card__details__user");
+const cardExpiration = document.querySelector(".card__details__expiration");
+const cardCode = document.querySelector(".card__code");
+
+//FORM PROPERTIES
+const form = document.querySelector(".form");
+const formFieldInput = document.querySelectorAll(".form__field__input");
+
+//BUTTONS
+const confirmBtn = document.querySelector(".btn--confirm");
+const dismissBtn = document.querySelector(".btn--dismiss");
+
